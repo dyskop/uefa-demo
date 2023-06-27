24e39ee173e7d83a016e9fc347dcfdbe6e586e27
Dzmitry Skopinau <dimaskop18@gmail.com>
2023-06-28 00:26:51 +0200

    Initial commit

---
+++ src/main/java/by/skopinau/uefademo/UefaDemoApplication.java
@@ -0,0 +1,13 @@
+package by.skopinau.uefademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UefaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UefaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/skopinau/uefademo/UefaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package by.skopinau.uefademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UefaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
